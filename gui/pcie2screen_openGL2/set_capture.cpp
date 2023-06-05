60981cfc91c4f837af0f029adfebed742384b5a1
Andreas Zhang <denglitsch@gmail.com>
2023-06-05 14:33:08 +0800

    feat: intr trigger flush

---
+++ gui/pcie2screen_openGL2/set_capture.cpp
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include "xdma_programe.h"
+#include "set_capture.h"
+
+xdma_getImg::xdma_getImg(xdma_programe *pXdma, char *pRGB, pthread_mutex_t *pOptNotice)
+{
+    stateWorking = false;
+    this->pRGB = pRGB;
+    this->pXdma = pXdma;
+    this->pOptNotice = pOptNotice;
+    start();
+}
+
+void xdma_getImg::run()
+{
+    int i;
+
+    while(1)
+    {
+        if(stateWorking)
+        {
+            pthread_mutex_lock(pOptNotice);
+            pXdma->wait_for_s2mm_intr();
+            i = pXdma->read_pack(pRGB, 1920*1080*3);
+            if(i != 1920*1080*3)
+            {
+                qDebug("read one pack error %d", i);
+            }
+            emit flushImg();
+        }
+        else
+        {
+            usleep(100000);
+        }
+
+    }
+}
+
+void xdma_getImg::setstart(bool isRuning)
+{
+    stateWorking = isRuning;
+}
---
+++ gui/pcie2screen_openGL2/xdma_programe.h
@@ -0,0 +1,59 @@
+#ifndef XDMA_PROGRAME_H
+#define XDMA_PROGRAME_H
+
+#include <pthread.h>
+#include <sys/time.h>
+#include <QtWidgets>
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+#  define ltohl(x)       (x)
+#  define ltohs(x)       (x)
+#  define htoll(x)       (x)
+#  define htols(x)       (x)
+#elif __BYTE_ORDER == __BIG_ENDIAN
+#  define ltohl(x)     __bswap_32(x)
+#  define ltohs(x)     __bswap_16(x)
+#  define htoll(x)     __bswap_32(x)
+#  define htols(x)     __bswap_16(x)
+#endif
+#define MAP_SIZE (1024*1024*1024UL)
+#define S2MM_VDMA_BASE 0x04A30000
+#define VIDEO_FRAME_STORE_NUM 1
+#define VDMA_S2MM_CR 0x30
+#define VDMA_S2MM_SR 0x34
+#define VDMA_S2MM_ADDR1 0xAC
+#define VDMA_S2MM_ADDR2 0xB0
+#define VDMA_S2MM_ADDR3 0xB4
+#define VDMA_S2MM_HSIZE 0xA8
+#define VDMA_S2MM_STRID 0xA4
+#define VDMA_S2MM_VSIZE 0xA0
+class xdma_programe
+{
+public:
+    xdma_programe();
+    ~xdma_programe();
+    bool getDevice(void);
+    int read_pack(char *pData, int len);
+
+    int open_event(char *filename);
+    int open_control(char *filename);
+    void * mmap_control(int fd,long mapsize);
+    int wait_for_s2mm_intr();
+    int read_event(int fd);
+    void write_control(void *base_addr,int offset,uint32_t val);
+    int c2h_event_fd;
+    int control_fd;
+    void *control_base;
+
+private:
+    int dev_fd; // c2h_dma_fd
+};
+
+#endif // XDMA_PROGRAME_H
